5a00f4882943ea6a66673efbfb65486670a4899a
Abbas Elmas <abbaselmas@gmail.com>
2023-11-01 13:36:56 +0300

    -vgg +boost

--- main.py
+++ main.py
@@ -223,13 +223,13 @@ lucid = cv2.xfeatures2d.LUCID_create()
 latch = cv2.xfeatures2d.LATCH_create()
 beblid= cv2.xfeatures2d.BEBLID_create(5.0)
 teblid= cv2.xfeatures2d.TEBLID_create(5.0)
-vgg   = cv2.xfeatures2d.VGG_create()
+boost = cv2.xfeatures2d.BoostDesc_create()
+# vgg   = cv2.xfeatures2d.VGG_create()
 # daisy = cv2.xfeatures2d.DAISY_create()
-# boost = cv2.xfeatures2d.BoostDesc_create()
 # lists of the different detectors, descriptors and matching methods
 DetectDescript = list([sift, akaze, orb, brisk, kaze])
 Detectors     = list([fast, star, mser, agast, gftt, harrislaplace, msd, tbmr])
-Descriptors   = list([freak, brief, lucid, latch, beblid, teblid, vgg]) #boost, daisy])
+Descriptors   = list([freak, brief, lucid, latch, beblid, teblid, boost]) #vgg, daisy])
 matching2 = list([cv2.NORM_L1, cv2.NORM_L2])
 matching3 = list([cv2.NORM_L1, cv2.NORM_L2, cv2.NORM_HAMMING])
 # ................................................................................
@@ -406,13 +406,13 @@ print(f"Scenario 3 Elapsed time: {time.time() - scenario3_time} seconds")
 # Visualization of the results
 DetectDescript = list([sift, akaze, orb, brisk, kaze])
 Detectors     = list([fast, star, mser, agast, gftt, harrislaplace, msd, tbmr])
-Descriptors   = list([freak, brief, lucid, latch, beblid, teblid, vgg]) # boost, daisy])
+Descriptors   = list([freak, brief, lucid, latch, beblid, teblid, boost]) # vgg, daisy])
 # ..........................................................................................................................
 
 # Binary and non-binary methods used to set the legend
 DetectDescriptLegend = ['sift', 'akaze', 'orb', 'brisk', 'kaze']
 DetectorsLegend     = ['fast-', 'star-', 'mser-', 'agast-', 'gftt-', 'harrislaplace-', 'msd-', 'tbmr-']
-DescriptorsLegend   = ['freak', 'brief', 'lucid', 'latch', 'beblid', 'teblid', 'vgg'] # 'boost', 'daisy']
+DescriptorsLegend   = ['freak', 'brief', 'lucid', 'latch', 'beblid', 'teblid', 'boost'] # 'vgg', 'daisy']
 
 c2 = 1 # for non-binary methods "DetectDescript" (c2=0 for bf.L1, c2=1 for bf.L2)
 c3 = 2 # for binary methods "Detectors with Descriptors" (c2=0 for bf.L1, c2=1 for bf.L2, c2=2 for bf.HAMMING)
