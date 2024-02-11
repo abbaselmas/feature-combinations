2b976fe9fea2d026328fe6e9d30c04f1baa4a0a0
Abbas Elmas <abbaselmas@gmail.com>
2024-02-11 10:36:28 +0300

    second graph forgotten

--- display.py
+++ display.py
@@ -80,7 +80,7 @@ for i in range(len(DetectorsLegend)):
             color = f'rgba({i * 30}, {j * 20}, {(i + j) * 2}, 1)'  # Adjust as needed
             style = line_styles[j % len(line_styles)]  # Cycle through line styles
             x = ["Img2", "Img3", "Img4", "Img5", "Img6"]
-            if not Rate_graf[:, c3, i, j].any(np.nan) and not Rate_wall[:, c3, i, j].any(np.nan) and not Rate_trees[:, c3, i, j].any(np.nan) and not Rate_bikes[:, c3, i, j].any(np.nan):
+            if not np.isnan(Rate_graf[:, c3, i, j]).any() and not np.isnan(Rate_wall[:, c3, i, j]).any() and not np.isnan(Rate_trees[:, c3, i, j]).any() and not np.isnan(Rate_bikes[:, c3, i, j]).any():
                 legend_group = f'{DetectorsLegend[i]}-{DescriptorsLegend[j]}-{Norm[c3]}'  # Unique legend group for each trace
             trace_G = go.Scatter(x=x, y=Rate_G, mode='lines', line=dict(color=color, dash=style), name=f'{DetectorsLegend[i]}-{DescriptorsLegend[j]}-{Norm[c3]}', legendgroup=legend_group, showlegend= True)
             trace_W = go.Scatter(x=x, y=Rate_W, mode='lines', line=dict(color=color, dash=style), name='', legendgroup=legend_group, showlegend=False)
