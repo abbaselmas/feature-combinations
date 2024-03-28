b0373b7dcdbcabe2963838b975c00339f2da533a
Abbas Elmas <abbaselmas@gmail.com>
2024-03-28 12:20:37 +0300

    oxford dataset ppm files and Homography matrices for affine

---
+++ oxfordAffine/convert_ppm2png.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+
+# Function to convert PPM to PNG
+def convert_ppm_to_png(input_file, output_file):
+    try:
+        img = Image.open(input_file)
+        img.save(output_file, 'PNG')
+        print(f"Converted {input_file} to {output_file}")
+    except Exception as e:
+        print(f"Error converting {input_file}: {e}")
+
+# Get the current directory where the script is located
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Loop through all subdirectories and files in the script's directory
+for root, dirs, files in os.walk(script_directory):
+    for file in files:
+        if file.endswith('.ppm'):
+            ppm_path = os.path.join(root, file)
+            png_path = ppm_path.replace('.ppm', '.png')
+            convert_ppm_to_png(ppm_path, png_path)
